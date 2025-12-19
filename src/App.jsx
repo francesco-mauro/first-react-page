b2cafe6506946b89489cacd9cc3d4416acd2e864
francesco-mauro <francesco.mauro180@gmail.com>
2025-12-19 16:10:32 +0100

    add: COmponents and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // Immmage import statement
 import reactImg from './assets/react-core-concepts.png';
+import componentImg from './assets/components.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -9,6 +10,8 @@ function genRandomInt(max) {
 
 function Header() {
   const description = reactDescriptions[genRandomInt(2)];
+  //Test debug
+  console.log('Rendering Header with description:', description);
 
   return (
     <header>
@@ -21,12 +24,46 @@ function Header() {
   );
 }
 
+
+function CoreConcept(props) {
+  return <li>
+    <img src={props.img} alt="boh" />
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
+  </li>;
+}
+
 function App() {
   return (
-    <div>
-      <Header />
-      <main>
-        <h2>Time to get started!</h2>
+    <div>          
+      <Header /> 
+      <main>    
+        <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept 
+          title="Components" 
+          description="Core UI building blocks"
+          img={componentImg}
+          />
+            <CoreConcept 
+          title="Components" 
+          description="Core UI building blocks"
+          img={componentImg}
+          />
+            <CoreConcept 
+          title="Components" 
+          description="Core UI building blocks"
+          img={componentImg}
+          />
+            <CoreConcept 
+          title="Components" 
+          description="Core UI building blocks"
+          img={componentImg}
+          />
+        </ul>
+        </section>
+
       </main>
     </div>
   );
