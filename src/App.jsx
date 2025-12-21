a909a9a7c148e5137fe9ce8546770a2322db261e
francesco-mauro <francesco.mauro180@gmail.com>
2025-12-21 11:30:24 +0100

    feat: better structure and folder order

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import reactImg from './assets/react-core-concepts.png';
 import componentImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data';
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 // import { Header } from './components/Header';
 import CoreConcept from './components/CoreConcept.jsx';
 
@@ -10,21 +10,21 @@ import CoreConcept from './components/CoreConcept.jsx';
 
 function App() {
   return (
-    <div>          
-      <Header /> 
-      <main>    
+    <div>
+      <Header />
+      <main>
         <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-          <CoreConcept 
-          title={CORE_CONCEPTS[0].title} 
-          description={CORE_CONCEPTS[0].description}
-          image={CORE_CONCEPTS[0].image}
-          />
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
-        </ul>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
         </section>
 
       </main>
