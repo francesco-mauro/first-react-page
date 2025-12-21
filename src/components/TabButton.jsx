b5cb57ee54caa2b7bbefffad2f87cc62d28e82ba
francesco-mauro <francesco.mauro180@gmail.com>
2025-12-21 18:52:27 +0100

    feat: add isSelected prop to TabButton for active state indication

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
     console.log("TAB BUTTON COMPONENT RENDERING");
     return <li>
-        <button onClick={onSelect}>{children}</button>
+        <button className={isSelected ? "active" : ""} onClick={onSelect}>{children}</button>
         </li>
 }
 
