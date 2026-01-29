28f11a9f972de7a6f229a1ec036292c47098307c
francesco-mauro <francesco.mauro180@gmail.com>
2026-01-29 16:54:50 +0100

    feat: new Examples and Tabs

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -36,7 +36,6 @@ export default function Examples() {
   return (
     <Section title="Examples" id='examples'>
       <Tabs 
-      buttonsContainer="menu"
       buttons={
         <>
           <TabButton isSelected={selectedTopic === "components"} onClick={() => handleSelect("components")}> Components </TabButton>
